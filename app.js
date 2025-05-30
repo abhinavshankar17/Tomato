e35e33575227e0c631fd62f39c66ad5c5c60c6a2
abhinavshankar17 <abhinav.shankar17@gmail.com>
2025-05-31 02:42:22 +0530

    i-packages

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ const path = require('path');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const expressLayouts = require('express-ejs-layouts');
+const User = require('./models/users'); 
+
+mongoose.connect('mongodb://localhost:27017/Tomato')
+.then(() => {
+    console.log("MongoDB connection established.");
+})
+.catch(err => {
+    console.error("MongoDB connection error:", err);
+});
+
+
+app.use(express.urlencoded({ extended: true }));
 
 
 app.set('view engine', 'ejs');
@@ -16,6 +28,29 @@ app.use('/', (req , res) => {
     res.render('home');
 });
 
+app.get('/signup', (req, res) => {
+    res.render('signup');
+});
+
+app.post('/signup', async (req, res) => {
+    try {
+        const { username, email, phone, password } = req.body;
+
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.send("User already exists with this email.");
+        }
+
+        const newUser = new User({ username, email, phone, password });
+        await newUser.save();
+
+        res.send("User registered successfully!");
+        // Or: res.redirect('/'); to go to home page
+    } catch (err) {
+        console.error(err);
+        res.send("Error registering user.");
+    }
+});
 
 
 
